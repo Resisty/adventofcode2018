8daf7360339442e285a4c68babc7199bbf1d5a30
Brian Auron <briauron@amazon.com>
2018-12-17 11:00:07 -0800

    Adding progress on 7.2. Not working yet, although test input is passing.

--- day07.py
+++ day07.py
@@ -9,6 +9,8 @@ import argparse
 import pprint
 from queue import PriorityQueue, Empty
 
+MAX_WORKERS = 5
+DEFAULT_SECONDS = 60
 
 class Node:
     """ Node class for DAG
@@ -16,6 +18,7 @@ class Node:
     def __init__(self, name):
         self.name = name
         self.visited = False
+        self.seconds = ord(name) - ord('A') + DEFAULT_SECONDS + 1
         self.before = []
         self.after = []
     def __hash__(self):
@@ -92,7 +95,62 @@ def part2(args):
     """ Solve the puzzle
     """
     inpt = validate_and_open(args.input)
-    return "not done yet"
+    nodes = KeyDefaultdict(Node)
+    for line in inpt.strip().split('\n'):
+        grps = re.search(r'Step (\w+) must be finished before step (\w+) can begin.', line)
+        bfr, aftr = grps.groups()
+        nodes[bfr].after = sorted(list(set(nodes[bfr].after + [nodes[aftr]])))
+        nodes[aftr].before = sorted(list(set(nodes[aftr].before + [nodes[bfr]])))
+    queue = PriorityQueue()
+    workers = []
+    for _, node in nodes.items():
+        if not node.before:
+            queue.put(node)
+    print('Nodes without prior tasks:')
+    print([i.name for i in queue.__dict__['queue']])
+    node = queue.get()
+    total = 0
+    answer = ''
+    workers.append(node)
+    print(f'Selected node {node.name} because it is first alphabetically.')
+    while workers:
+        shortest_work = min(workers, key=lambda x: x.seconds)
+        elapsed = shortest_work.seconds
+        print(f'Workers in the queue: {[i.name for i in workers]}')
+        print(f'Worker {shortest_work.name} next to complete in {elapsed} seconds.')
+        total += elapsed
+        to_pop = []
+        for worker in workers:
+            worker.seconds -= elapsed
+            print(f'Worker {worker.name} time left: {worker.seconds} seconds.')
+            if worker.seconds == 0:
+                worker.visited = True
+                to_pop.append(worker)
+                answer += worker.name
+                for nextnode in worker.after:
+                    if not nextnode.visited:
+                        queue.put(nextnode)
+                        print(f"Adding {worker.name}'s next hops to the queue:")
+                        print([i.name for i in queue.__dict__['queue']])
+        for pop_me in to_pop:
+            workers.remove(pop_me)
+        print(f'Some workers finished. Answer updated to {answer}')
+        while len(workers) < MAX_WORKERS:
+            try:
+                i = queue.get_nowait()
+            except Empty:
+                break
+            print(f'Got node from front of queue: {i.name}')
+            if any([not j.visited for j in i.before]):
+                print(f"Removing {i.name} from queue because dependent tasks have not been visited yet")
+                continue
+            print(f'Selected node {i.name} because it is first alphabetically.')
+            if i.visited:
+                print(f'Already visited {i.name}, skipping.')
+                continue
+            workers.append(i)
+            print(f'Starting work on {i.name} for {i.seconds} seconds')
+    return total
 
 
 def validate_and_open(path):
@@ -118,4 +176,7 @@ if __name__ == '__main__':
     PART02.set_defaults(func=part2)
 
     ARGS = PARSER.parse_args()
+    if 'test' in ARGS.input:
+        MAX_WORKERS = 2
+        DEFAULT_SECONDS = 0
     print(ARGS.func(ARGS))
